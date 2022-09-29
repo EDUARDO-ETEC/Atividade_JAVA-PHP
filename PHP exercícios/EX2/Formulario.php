4f81bfa2b7729ff076c96a256177620bed572bc7
PoKsRammerfell <78971152+EDUARDO-ETEC@users.noreply.github.com>
2022-09-29 17:04:45 -0300

    Add files via upload

---
+++ PHP exercícios/EX2/Formulario.php
@@ -0,0 +1,23 @@
+<form action="processa.php">
+ <p>
+ <strong>O que você achou do site??</strong>
+ <dl><dd>
+ <input type="radio" name="avaliacao" value="muitobom" checked>Muito bom
+ <input type="radio" name="avaliacao" value="bom">Bom
+ <input type="radio" name="avaliacao" value="regular">Regular
+ <input type="radio" name="avaliacao" value="umlixo">Um Lixo
+ </dd></dl>
+ </p>
+ <p>
+ <strong>Qual a seção que você mais gostou??</strong>
+ <dl><dd>
+ <select name="secao" size"1">
+ <option value="emcartaz">Em cartaz</option>
+ <option value="trilhasonora">Trilha Sonora</option>
+ <option value="fotos">Galeria de Fotos</option>
+ <option value="bilheteria">Bilheteria</option>
+ <option value="outra">Outra</option>
+ </select>
+ Outra: <input type="text" size="26" maxlength="256" name="outra">
+ </dd></dl>
+ </p> 
